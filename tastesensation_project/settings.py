b38653f4a8363c7b451d881b51027bf1d78a3fe1
Sophie Thomson <sophielbenoit@gmail.com>
2024-09-19 16:57:27 +0000

    Generate new SECRET_KEY and move to env.py

--- tastesensation_project/settings.py
+++ tastesensation_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+6^34^yim$)&u_1y3v2%-*0_@@_zp7q+yn#+txh09)dxemtcp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
