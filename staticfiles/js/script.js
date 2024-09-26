72012fd02a578c52e5017b7c8e13dcd0d7567559
Sophie Thomson <sophielbenoit@gmail.com>
2024-09-26 18:41:27 +0000

    Collect static files for deployment

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,16 +1,23 @@
 console.log("Life, The Universe and Everything!");
 
+const ratingForm = document.getElementById("rating-form");
+const editRatingBtn = document.getElementById("edit-rating-btn");
 
-const rate = (rating, recipe_id) => {
-    fetch(`/rate/${recipe_id}/${rating}/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(rest => {
-        window.location.reload();
-                // you may want to update the rating here
-                // to simplify stuff, I just reload the page
-    })
+/** Removes the .hidden class from rating form so it will display */
+function show_rating_form() {
+ 
+    ratingForm.classList.remove("hidden");
+
+    hide_rating_button()
+    
 }
-  
+
+
+/** Adds the .hidden class to Edit Rating button so it is hidden */
+function hide_rating_button() {
+    
+    editRatingBtn.classList.add("hidden");
+    
+}
+
+ 
