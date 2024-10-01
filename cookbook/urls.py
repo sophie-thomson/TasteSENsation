9111472fb9fceb5ae154053ebe0733439287b577
Sophie Thomson <sophielbenoit@gmail.com>
2024-10-01 09:41:00 +0000

    Add delete CRUD functionality to comments

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<slug:slug>/', views.recipe_detail, name="recipe_detail"),
     path('<slug:slug>/comment_edit/<int:comment_id>', 
         views.comment_edit, name="comment_edit"),
+    path('<slug:slug>/comment_delete/<int:comment_id>',
+        views.comment_delete, name="comment_delete"),
 ] 
