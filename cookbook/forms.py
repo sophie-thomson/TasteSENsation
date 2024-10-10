0dd60aaaa12aa603dcd3c04de47e463ce61b58a9
Sophie Thomson <sophielbenoit@gmail.com>
2024-10-10 13:23:27 +0000

    Adjust forms.py to set initial content with html in ingredients and instructions fields

--- cookbook/forms.py
+++ cookbook/forms.py
@@ -47,13 +47,15 @@ class RecipeForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(RecipeForm, self).__init__(*args, **kwargs)
         self.fields['original_recipe'].widget.attrs.update({ 'placeholder':
-                "If your recipe is based on another recipe, add the link here" }),
-        self.fields['ingredients'].widget.attrs.update({ 'placeholder': 
-                "List all the ingredients, including any measurements"}),
-        self.fields['instructions'].widget.attrs.update({ 'placeholder':
-                "Provide simple step by step instructions"}),
+                "If your recipe is based on another recipe, add the link here" })
+        # Sets inital content for ingredients field to be read by Summernote widget
+        self.fields['ingredients'].initial = "<ul><li>Item 1</li><li>Item 2</li><li>Item 3</li></ul>"
+
+        self.fields['instructions'].initial = "<p>1. Describe step 1</p><hr><p>2.  Describe step 2</p><hr><p>3.  Describe step 3</p><hr>"
+
         self.fields['featured_image'].widget.attrs.update({ 'placeholder':
-                "{% if recipe.featured_image %}See current uploaded image below{% endif %}"}),
+                "{% if recipe.featured_image %}See current uploaded image below{% endif %}"})
+
         self.fields['featured_image'].options={ 
             'tags': "directly_uploaded",
             'format': "WEBP",
